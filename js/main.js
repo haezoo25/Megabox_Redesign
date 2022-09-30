831e7224c7205531fbb3a46ff979b8ff2f904bd4
haezoo <haezoo25@gmail.com>
2022-09-30 17:25:08 +0900

    redesign

---
+++ js/main.js
@@ -0,0 +1,120 @@
+new fullpage('#full-page', {
+    licensekey: '',
+    sectionsColor: ['#101011', '#101011', '#101011', '#101011', '#101011', '#101011'],
+    navigation: true,
+    navigationTooltips:['Main', 'Movie', 'Curation', 'Benefit', 'Event', 'Brand video'],
+    scrollingSpeed: 2000,
+    onLeave: function(origin,destination, direction) {
+        console.log('onLeave', origin.index, destination.index);
+    },
+    afterLoad: function(origin,destination, direction) {
+        if(destination.index == 1) {
+            $('.s1 h5').show();
+        }
+    }
+});
+// 마우스
+let mouseCursor = document.querySelector(".cursor");
+let navLinks = document.querySelectorAll(".gnb li a"); //메뉴 링크
+//window 객체에 scroll & mouse 이벤트를 추가하고 cursor함수 실행되도록 함
+window.addEventListener("scroll", cursor);
+window.addEventListener("mousemove", cursor);
+//커스텀 커서의 left값과 top값을 커서의 XY좌표값과 일치시킴
+function cursor(e) {
+  mouseCursor.style.left = e.pageX + "px";
+  mouseCursor.style.top = e.pageY - scrollY + "px";
+}
+
+// 헤더 메뉴
+$(function() {
+	$('.hamburger-button').on('click', function(event){
+		event.preventDefault();
+		
+		$(this).toggleClass('active');
+        $('.overlay').toggleClass('visible');
+
+	});
+});
+
+// 영화 슬라이드
+$(document).ready(function(){
+
+    let carousel = $(".gallery-container").waterwheelCarousel({
+        flankingItems: 5, // 몇 개의 컨텐츠를 보여줄 건지 설정
+        separation: 250, // 5개 컨텐츠의 사이 설정
+        speed: 1000, // 속도 설정
+        opacityMultiplier: 1, // 양쪽 2개 opacity 값 설정
+        sizeMultiplier: 0.8, // 양쪽 2개 컨텐츠의 크기 설정
+        separationMultiplier: 0.5 //양쪽 2개 서로와의 사이 거리 설정
+    });
+    // $('#carousel_prev').bind('click', function () {
+    //     carousel.prev();
+    //     return false
+    // });
+    // $('#carousel_next').bind('click', function () {
+    //     carousel.next();
+    //     return false;
+    // });
+
+
+// s2 소사이어티
+    $("body").on("mousemove",function(e){
+        let offset = $(this).offset();
+        let scrTop = e.pageY - offset.top;
+        let scrLeft = e.pageX - offset.left;
+
+        $('.s2 .step1 img').css({top:(scrTop * 0.1), left:(scrLeft * 0.1),});
+        $('.s2 .step2 img').css({top:(scrTop * 0.1), left:(scrLeft * 0.1),});
+
+    });
+    $(".s2 h4").on("click",function(){
+        $(".s2 h5, .s2 .step2").removeClass("on");
+        $(".s2 h4, .s2 .step1").addClass("on");
+
+
+    });
+    $(".s2 h5").on("click",function(){
+       $(".s2 h4, .s2 .step1").removeClass("on");
+       $(".s2 h5, .s2 .step2").addClass("on");
+
+
+    });
+
+    // s2 소사이어티 내용변경
+    $(".s2 h4").on("click",function(){
+        $(".s2 .classic").removeClass("on");
+        $(".s2 .film").addClass("on");
+
+
+    });
+    $(".s2 h5").on("click",function(){
+       $(".s2 .film").removeClass("on");
+       $(".s2 .classic").addClass("on");
+
+
+    });
+
+// s3 혜택
+    $(".s3 .arrow").on("click", function (){
+        if( $(this).hasClass("on")){
+            $(this).removeClass("on");
+            $(".s3 .benefit-category").removeClass("on");
+        } else {
+            $(this).addClass("on");
+            $(".s3 .benefit-category").addClass("on");
+        }
+    });
+
+
+
+
+});
+
+
+
+
+
+
+
+
+
---
+++ js/vender/carousel.js
@@ -0,0 +1,97 @@
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'add', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+  constructor(container, items, controls) {
+    this.carouselContainer = container;
+    this.carouselControls = controls;
+    this.carouselArray = [...items];
+  }
+
+  // Update css classes for gallery
+  updateGallery() {
+    this.carouselArray.forEach(el => {
+      el.classList.remove('gallery-item-1');
+      el.classList.remove('gallery-item-2');
+      el.classList.remove('gallery-item-3');
+      el.classList.remove('gallery-item-4');
+      el.classList.remove('gallery-item-5');
+    });
+
+    this.carouselArray.slice(0, 5).forEach((el, i) => {
+      el.classList.add(`gallery-item-${i+1}`);
+    });
+  }
+
+  // Update the current order of the carouselArray and gallery
+  setCurrentState(direction) {
+
+    if (direction.className == 'gallery-controls-previous') {
+      this.carouselArray.unshift(this.carouselArray.pop());
+    } else {
+      this.carouselArray.push(this.carouselArray.shift());
+    }
+    
+    this.updateGallery();
+  }
+
+  // Construct the carousel navigation
+  // setNav() {
+    // galleryContainer.appendChild(document.createElement('ul')).className = 'gallery-nav';
+
+    // this.carouselArray.forEach(item => {
+    //   const nav = galleryContainer.lastElementChild;
+    //   nav.appendChild(document.createElement('li'));
+    // }); 
+  // }s
+
+  // Construct the carousel controls
+  setControls() {
+    this.carouselControls.forEach(control => {
+      galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+
+      document.querySelector(`.gallery-controls-${control}`).innerText = control;
+    });
+  }
+ 
+  // Add a click event listener to trigger setCurrentState method to rearrange carousel
+  useControls() {
+    const triggers = [...galleryControlsContainer.childNodes];
+
+    triggers.forEach(control => {
+      control.addEventListener('click', e => {
+        e.preventDefault();
+
+        if (control.className == 'gallery-controls-add') {
+          const newItem = document.createElement('img');
+          const latestItem = this.carouselArray.length;
+          const latestIndex = this.carouselArray.findIndex(item => item.getAttribute('data-index') == this.carouselArray.length)+1;
+
+          // Assign the necessary properties for new gallery item
+          Object.assign(newItem,{
+            className: 'gallery-item',
+            src: `http://fakeimg.pl/300/?text=${this.carouselArray.length+1}`
+          });
+          newItem.setAttribute('data-index', this.carouselArray.length+1);
+
+          // Then add it to the carouselArray and update the gallery
+          this.carouselArray.splice(latestIndex, 0, newItem);
+          document.querySelector(`[data-index="${latestItem}"]`).after(newItem);
+          this.updateGallery();
+
+        } else {
+          this.setCurrentState(control);
+        }
+
+      });
+    });
+  }
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+// exampleCarousel.setNav();
+exampleCarousel.useControls();
